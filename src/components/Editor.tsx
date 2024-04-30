55f522df7a7feefb77df0156be2934e4226329a7
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-05-01 00:18:10 +0530

    fixed resize issue for the editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,12 +5,12 @@ interface EditorProps {
 
 export default function Editor({ markdown, setMarkdown }: EditorProps) {
   return (
-    <div className="overflow-y-hidden border border-r-2 border-gray-700">
+    <div className="overflow-y-hidden border border-r-2 border-gray-700 h-full">
       {/* <div className="w-full bg-gray-950 p-3 uppercase text-gray-400 tracking-widest">
         Markdown
       </div> */}
       <textarea
-        className="w-full h-screen overflow-y-scroll outline-none bg-transparent p-6"
+        className="w-full h-screen overflow-y-scroll outline-none bg-transparent p-6 resize-none"
         onChange={(e) => setMarkdown(e.target.value)}>
         {markdown}
       </textarea>
